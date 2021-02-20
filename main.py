8de3c6e0295a57d59da1666e6a6ff4829fd5d942
Vojtech Kaiser <vojtechkaiser95@gmail.com>
2021-02-20 15:31:54 +0100

    New condition and more lines added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print('Hello World!')
 
 print('I just entered the branch \'develop\'')
+
+x = 1
+
+if -1 != x:
+    print('Neni rovno!!!')
+
+print('Nachazim se v novem developu, ten stary uz jsem smazal!')
+
+print('Neco jsem dodelat, ale jeste nepridal')
